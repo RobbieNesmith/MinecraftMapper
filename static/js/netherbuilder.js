29d1d52d0c65908ff6c05588ece811fbf11152d0
Robbie Nesmith <bobberto1995@gmail.com>
2020-03-31 09:06:43 -0500

    Add button for Nether and Overworld views

--- static/js/netherbuilder.js
+++ static/js/netherbuilder.js
@@ -1,4 +1,7 @@
-let map = {};
+let map = undefined;
+let netherLayerGroup = undefined;
+let overworldLayerGroup = undefined;
+let dimension = "nether";
 
 function generateVertexPopup(vertex) {
   let popupContainer = document.createElement("div");
@@ -124,24 +127,74 @@ function populateEditPathForm(path) {
   editPathForm.secondz.value = path.end[1];
 }
 
-function renderVertexData(map, vertexJson) {
+function resetLayerGroups() {
+  if (netherLayerGroup) {
+    netherLayerGroup.off();
+  }
+  if (overworldLayerGroup) {
+    overworldLayerGroup.off();
+  }
+  netherLayerGroup = new L.LayerGroup();
+  overworldLayerGroup = new L.LayerGroup();
+}
+
+function renderVertexData(map, vertexJson, dimension) {
   for (let vertex of vertexJson.vertices) {
-    let coords = L.latLng({lng: vertex.netherCoords[0], lat: vertex.netherCoords[1]});
-    let marker = L.marker(coords, {title: vertex.name}).addTo(map);
-    marker.bindPopup(generateVertexPopup(vertex));
+    let netherCoords = L.latLng({lng: vertex.netherCoords[0], lat: vertex.netherCoords[1]});
+    let overworldCoords = L.latLng({lng: vertex.overworldCoords[0], lat: vertex.overworldCoords[1]});
+    let netherMarker = L.marker(netherCoords, {title: vertex.name});
+    let overworldMarker = L.marker(overworldCoords, {title: vertex.name});
+    netherMarker.bindPopup(generateVertexPopup(vertex));
+    overworldMarker.bindPopup(generateVertexPopup(vertex));
+    netherLayerGroup.addLayer(netherMarker);
+    overworldLayerGroup.addLayer(overworldMarker);
   }
 }
 
-function renderPathData(map, pathJson) {
+function renderPathData(map, pathJson, dimension) {
   for (let path of pathJson.paths) {
     let start = L.latLng({lng: path.start[0], lat: path.start[1]});
     let end = L.latLng({lng: path.end[0], lat: path.end[1]});
-    let pathSeg = L.polyline([start, end]).addTo(map);
+    let pathSeg = L.polyline([start, end]);
     pathSeg.bindPopup(generatePathPopup(path));
+    netherLayerGroup.addLayer(pathSeg);
+  }
+}
+
+function toggleDimension() {
+  let toggleDimensionButton = document.getElementById("toggleDimension");
+  let zoom = map.getZoom();
+  let center = map.getCenter();
+  if (dimension === "nether") {
+    dimension = "overworld";
+    toggleDimensionButton.innerText = "Show Nether Locations";
+    toggleDimensionButton.className = "btn btn-danger";
+    zoom = zoom - 3;
+    center.lat = center.lat * 8;
+    center.lng = center.lng * 8;
+    netherLayerGroup.removeFrom(map);
+    overworldLayerGroup.addTo(map);
+  } else {
+    dimension = "nether";
+    toggleDimensionButton.innerText = "Show Overworld Locations";
+    toggleDimensionButton.className = "btn btn-success";
+    zoom = zoom + 3
+    center.lat = center.lat / 8;
+    center.lng = center.lng / 8;
+    overworldLayerGroup.removeFrom(map);
+    netherLayerGroup.addTo(map);
+  }
+  map.setView(center, zoom);
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("dimension", dimension);
   }
 }
 
 function setupMap() {
+  if (map) {
+    map.off();
+    map.remove();
+  }
   
   let theCrs = L.CRS.Simple;
   
@@ -149,11 +202,12 @@ function setupMap() {
   
   let center = {lat: 0, lng: 0};
   let zoom = 13;
-  
+
   if (typeof(Storage) !== "undefined") {
     let storedCenter = localStorage.getItem("center");
     let storedZoom = localStorage.getItem("zoom");
-    
+    let storedDimension = localStorage.getItem("dimension");
+
     if (storedCenter) {
       try {
         center = JSON.parse(storedCenter);
@@ -170,14 +224,24 @@ function setupMap() {
         localStorage.removeItem("zoom");
       }
     }
+    if (storedDimension) {
+      dimension = storedDimension;
+      let toggleDimensionButton = document.getElementById("toggleDimension");
+      if (dimension === "nether") {
+        toggleDimensionButton.innerText = "Show Overworld Locations";
+        toggleDimensionButton.className = "btn btn-success";
+      } else {
+        toggleDimensionButton.innerText = "Show Nether Locations";
+        toggleDimensionButton.className = "btn btn-danger";
+      }
+    }
   }
-  
   let mapOptions = {
       center: center,
       zoom: zoom,
       crs: theCrs
   };
-  
+
   map = L.map('map', mapOptions);
   
   if (typeof(Storage) !== "undefined") {
@@ -198,6 +262,8 @@ function setupMap() {
 
   L.simpleGraticule(graticuleOptions).addTo(map);
   
+  resetLayerGroups();
+
   fetch("/api/vertices/list")
       .then(res => res.json())
       .then(json => {
@@ -209,4 +275,10 @@ function setupMap() {
       .then(json => {
         renderPathData(map, json);
       });
+
+  if (dimension === "nether") {
+    netherLayerGroup.addTo(map);
+  } else {
+    overworldLayerGroup.addTo(map);
+  }
 }
